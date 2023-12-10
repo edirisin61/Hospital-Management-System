92b89ee5d212d44cdcffdc960203f4b3e96bb5b1
Kavinda Edirisingha <kavindaedirisingha10@gmail.com>
2023-12-10 20:06:47 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,69 @@
+const showLinksButton1 = document.getElementById("showLinksButton-one");
+const linksContainer1 = document.getElementById("linksContainer-one");
+let hoverTimeOut1;
+
+showLinksButton1.addEventListener("mouseover", function() {
+    clearTimeout(hoverTimeOut1);
+    linksContainer1.style.display = "block";
+});
+
+showLinksButton1.addEventListener("mouseout", function() {
+    clearTimeout(hoverTimeOut1);
+    hoverTimeOut1 = setTimeout(function() {
+        linksContainer1.style.display = "none";
+    }, 2000);
+});
+
+const showLinksButton2 = document.getElementById("showLinksButton-two");
+const linksContainer2 = document.getElementById("linksContainer-two");
+let hoverTimeOut2;
+
+showLinksButton2.addEventListener("mouseover", function() {
+    clearTimeout(hoverTimeOut2);
+    linksContainer2.style.display = "block";
+});
+
+showLinksButton2.addEventListener("mouseout", function() {
+    clearTimeout(hoverTimeOut2);
+    hoverTimeOut2 = setTimeout(function() {
+        linksContainer2.style.display = "none";
+    }, 2000);
+});
+
+const showLinksButton3 = document.getElementById("showLinksButton-three");
+const linksContainer3 = document.getElementById("linksContainer-three");
+let hoverTimeOut3;
+
+showLinksButton3.addEventListener("mouseover", function() {
+    clearTimeout(hoverTimeOut3);
+    linksContainer3.style.display = "block";
+});
+
+showLinksButton3.addEventListener("mouseout", function() {
+    clearTimeout(hoverTimeOut3);
+    hoverTimeOut3 = setTimeout(function() {
+        linksContainer3.style.display = "none";
+    }, 2000);
+});
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
+
+
+
